7e3939551d2d9500e5c127d35da7460d0c274fef
Kesh1354 <162817648+Kesh1354@users.noreply.github.com>
2026-08-30 09:32:36 +0530

    Add files via upload

---
+++ data/movies.js
@@ -0,0 +1,20 @@
+const movies = [
+    {
+        title: "All We Imagine as Light",
+        folder: "All We Imagine as Light",
+        images: 4,
+        director: ["Payal Kapadia"],
+        cinematographer: ["Ranabir Das","Payal Kapadia"],
+        year: 2024,
+        aspectRatios: ["1.66:1"]
+    },
+    {
+        title: "First Man",
+        folder: "First Man",
+        images: 20,
+        director: ["Damien Chazelle"],
+        cinematographer: ["Linus Sandgren"],
+        year: 2018,
+        aspectRatios: ["2.39:1", "1.78:1","1.66:1"]
+    },
+];
